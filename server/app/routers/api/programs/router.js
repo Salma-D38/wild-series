349bb5fd9205cc79cba3d63ea9043f9869b3fa98
Salma DESMAZIERES <s.desmazieres@yahoo.fr>
2024-05-22 16:27:00 +0200

    finished last step

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -3,10 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 // Import item-related actions
-const { browse} = require("../../../controllers/programActions");
+const { browse, read} = require("../../../controllers/programActions");
+
+
 
 // Route to get a list of items
 router.get("/", browse);
+router.get("/:id", read); // Complete path is: /api/programs/:id
 
 /* ************************************************************************* */
 
